ab2ea90c1804efbaea100b39880474ef2d90f66f
abdou aziz sokhna <azizsokhnamboup1o@gmail.com>
2025-05-01 21:17:42 +0000

    frontend startde

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,8 +1,6 @@
 import './assets/main.css'
-import router from './router' // ou simplement './router' si le fichier est index.js
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+createApp(App).mount('#app')
